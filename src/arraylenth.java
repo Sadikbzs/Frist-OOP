fd937a37a6f7ffb170dd06c212be0e403619f86b
MD ABDULLAH AL SADIK <sadikbzs20@gmail.com>
2025-07-31 21:59:08 +0600

    2nd problem

---
+++ src/arraylenth.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class arraylenth {
+    public static void main (String args[]){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+     int []arr= new int [n];
+     System.out.println(arr.length);
+     }
+    }
+
